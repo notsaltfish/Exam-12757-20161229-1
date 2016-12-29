afd8b8a8b18a218a749b31413ff9fe716e6559b3
notsaltedfish <c1097210504@qq.com>
2016-12-29 17:02:22 +0800

    第一次提交

---
+++ src/main/java/com/chen/sm/App.java
@@ -0,0 +1,13 @@
+package com.chen.sm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
