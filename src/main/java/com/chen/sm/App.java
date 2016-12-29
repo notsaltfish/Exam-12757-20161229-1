25926fd574a688b7c80e1f28514c55a7dece292b
notsaltedfish <c1097210504@qq.com>
2016-12-29 19:15:09 +0800

    基本功能已经完成

--- src/main/java/com/chen/sm/App.java
+++ src/main/java/com/chen/sm/App.java
@@ -1,13 +1,49 @@
 package com.chen.sm;
 
+import java.util.Scanner;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.chen.sm.po.Film;
+import com.chen.sm.service.FilmService;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	static FilmService filmService;
+	static ClassPathXmlApplicationContext context = new 
+			ClassPathXmlApplicationContext("applicationContext.xml");
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	
+    	filmService =(FilmService) context.getBean("filmServiceImpl");
+    	context.start();
+        input();
+       context.stop();;
+    }
+    
+    public static  void input(){
+//    	while(true){
+//    		
+//    	}
+    	Film film = new Film();
+        Scanner scanner  = new Scanner(System.in);
+        String title = "";
+        String description  = "";
+        Byte languageId =0;
+        System.out.println("请输入电影名称【title】");
+        title = scanner.nextLine();
+        System.out.println("请输入电影名称【description】");
+        description = scanner.nextLine();
+        System.out.println("请输入语言ID【languageId】");
+        languageId = scanner.nextByte();
+        film.setLanguageId(languageId);
+        film.setTitle(title);
+        film.setDescription(description);
+        filmService.add(film);
+    	
     }
 }
