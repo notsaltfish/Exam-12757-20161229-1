f4af7fb5a88f656aa62670e3cf3ebd4996293cd5
notsaltedfish <c1097210504@qq.com>
2016-12-29 21:45:46 +0800

    xiugai

--- src/main/java/com/chen/sm/App.java
+++ src/main/java/com/chen/sm/App.java
@@ -77,10 +77,12 @@ public class App
     public    void getTitle(Film film){
     	System.out.println("请输入电影名称【title】");
     	String title = "";
+    	 scanner = new Scanner(System.in);
     	try{
     	    title = scanner.nextLine();
 			if(title.trim().equals("")){			
 				getTitle(film);
+				return ;
 			}
 			film.setTitle(title);
 		}catch(Exception e){
@@ -91,10 +93,12 @@ public class App
     public    void getDescription(Film film){
     	   System.out.println("请输入电影名称【description】");
         String description  = "";
+        scanner = new Scanner(System.in);
     	try{
     		description = scanner.nextLine();
 			if(description.trim().equals("")){			
 				getDescription(film);
+				return ;
 			}
 			film.setDescription(description);
 		}catch(Exception e){
@@ -105,10 +109,12 @@ public class App
     public  void isEnd(){
     	System.out.println("输入1继续插入数据，输入2结束程序");
     	int num = -1;
+    	 scanner = new Scanner(System.in);
     	try{
     		num = Integer.parseInt(scanner.nextLine());
     		if(!ints.contains(num)){
     			isEnd();
+    			return ;
     		}
     	}catch(Exception e){
 			isEnd();
@@ -121,11 +127,13 @@ public class App
     public    void getLanguageId(Film film){
     	System.out.println("请输入位于(1,2,3,4,5,6)当中的数字");
     	Byte languageId=null;
+    	 scanner = new Scanner(System.in);
     	try{
     	
 		languageId = Byte.parseByte(scanner.nextLine());
 			if(!sets.contains(languageId)){			
 				getLanguageId(film);
+				return ;
 			}
 			film.setLanguageId(languageId);
 		}catch(Exception e){
